37f2a6a3eede66ceaa02b548d8565b483c76c16d
dziubenzo <dziubenzo@gmail.com>
2023-12-09 17:20:11 +0100

    Refactor Location class constructor
    Finally, it is pleasant to look at and that's what I should have done
    from the start (use a loop and dynamic property creation to fill a
    class instance with API data). Another lesson learned!

--- src/js/location.js
+++ src/js/location.js
@@ -1,5 +1,6 @@
 export default class Location {
   constructor(data) {
+    // Divide API data into relevant chunks
     const location = data.location;
     const current = data.current;
     const today = data.forecast.forecastday[0].day;
@@ -11,6 +12,11 @@ export default class Location {
     const twoDays = data.forecast.forecastday[2].day;
     const twoDaysAstro = data.forecast.forecastday[2].astro;
     const twoDaysDate = data.forecast.forecastday[2].date;
+
+    const dayDataArray = [today, tomorrow, twoDays];
+    const astroDataArray = [todayAstro, tomorrowAstro, twoDaysAstro];
+    const dateDataArray = [todayDate, tomorrowDate, twoDaysDate];
+    const daysArray = ['today', 'tomorrow', 'twoDays'];
 
     this.location = location.name;
     this.country = location.country;
@@ -43,94 +49,57 @@ export default class Location {
     this.current.fahrenheit.feelsLike = current.feelslike_f + ' °F';
     this.current.fahrenheit.visibility = current.vis_miles + ' miles';
 
-    // Today forecast data
-    this.today = {};
-    this.today.weatherDescription = today.condition.text;
-    this.today.weatherCode = today.condition.code;
-    this.today.sunrise = new Date(`${todayDate} ${todayAstro.sunrise}`);
-    this.today.sunset = new Date(`${todayDate} ${todayAstro.sunset}`);
-    this.today.totalSnow = today.totalsnow_cm + ' cm';
-    this.today.averageHumidity = today.avghumidity + '%';
-    this.today.chanceOfRain = today.daily_chance_of_rain + '%';
-    this.today.chanceOfSnow = today.daily_chance_of_snow + '%';
-    this.today.uvIndex = today.uv;
-    this.today.airQualityIndex = today.air_quality['gb-defra-index'];
+    // Fill an instance with forecast data dynamically for all days
+    for (let index = 0; index < dayDataArray.length; index++) {
+      // Forecast data
+      this[`${daysArray[index]}`] = {};
+      this[`${daysArray[index]}`].weatherDescription =
+        dayDataArray[index].condition.text;
+      this[`${daysArray[index]}`].weatherCode =
+        dayDataArray[index].condition.code;
+      this[`${daysArray[index]}`].sunrise = new Date(
+        `${dateDataArray[index]} ${astroDataArray[index].sunrise}`,
+      );
+      this[`${daysArray[index]}`].sunset = new Date(
+        `${dateDataArray[index]} ${astroDataArray[index].sunset}`,
+      );
+      this[`${daysArray[index]}`].totalSnow =
+        dayDataArray[index].totalsnow_cm + ' cm';
+      this[`${daysArray[index]}`].averageHumidity =
+        dayDataArray[index].avghumidity + '%';
+      this[`${daysArray[index]}`].chanceOfRain =
+        dayDataArray[index].daily_chance_of_rain + '%';
+      this[`${daysArray[index]}`].chanceOfSnow =
+        dayDataArray[index].daily_chance_of_snow + '%';
+      this[`${daysArray[index]}`].uvIndex = dayDataArray[index].uv;
+      this[`${daysArray[index]}`].airQualityIndex =
+        dayDataArray[index].air_quality['gb-defra-index'];
 
-    // Today forecast data - Celsius
-    this.today.celsius = {};
-    this.today.celsius.maximumTemperature = today.maxtemp_c + ' °C';
-    this.today.celsius.minimumTemperature = today.mintemp_c + ' °C';
-    this.today.celsius.averageTemperature = today.avgtemp_c + ' °C';
-    this.today.celsius.maximumWindSpeed = today.maxwind_kph + ' km/h';
-    this.today.celsius.totalPrecipitation = today.totalprecip_mm + ' mm';
+      // Forecast data - Celsius
+      this[`${daysArray[index]}`].celsius = {};
+      this[`${daysArray[index]}`].celsius.maximumTemperature =
+        dayDataArray[index].maxtemp_c + ' °C';
+      this[`${daysArray[index]}`].celsius.minimumTemperature =
+        dayDataArray[index].mintemp_c + ' °C';
+      this[`${daysArray[index]}`].celsius.averageTemperature =
+        dayDataArray[index].avgtemp_c + ' °C';
+      this[`${daysArray[index]}`].celsius.maximumWindSpeed =
+        dayDataArray[index].maxwind_kph + ' km/h';
+      this[`${daysArray[index]}`].celsius.totalPrecipitation =
+        dayDataArray[index].totalprecip_mm + ' mm';
 
-    // Today forecast data - Fahrenheit
-    this.today.fahrenheit = {};
-    this.today.fahrenheit.maximumTemperature = today.maxtemp_f + ' °F';
-    this.today.fahrenheit.minimumTemperature = today.mintemp_f + ' °F';
-    this.today.fahrenheit.averageTemperature = today.avgtemp_f + ' °F';
-    this.today.fahrenheit.maximumWindSpeed = today.maxwind_mph + ' mph';
-    this.today.fahrenheit.totalPrecipitation = today.totalprecip_in + ' in';
-
-    // Tomorrow forecast data
-    this.tomorrow = {};
-    this.tomorrow.weatherDescription = tomorrow.condition.text;
-    this.tomorrow.weatherCode = tomorrow.condition.code;
-    this.tomorrow.sunrise = new Date(
-      `${tomorrowDate} ${tomorrowAstro.sunrise}`,
-    );
-    this.tomorrow.sunset = new Date(`${tomorrowDate} ${tomorrowAstro.sunset}`);
-    this.tomorrow.totalSnow = tomorrow.totalsnow_cm + ' cm';
-    this.tomorrow.averageHumidity = tomorrow.avghumidity + '%';
-    this.tomorrow.chanceOfRain = tomorrow.daily_chance_of_rain + '%';
-    this.tomorrow.chanceOfSnow = tomorrow.daily_chance_of_snow + '%';
-    this.tomorrow.uvIndex = tomorrow.uv;
-    this.tomorrow.airQualityIndex = tomorrow.air_quality['gb-defra-index'];
-
-    // Tomorrow forecast data - Celsius
-    this.tomorrow.celsius = {};
-    this.tomorrow.celsius.maximumTemperature = tomorrow.maxtemp_c + ' °C';
-    this.tomorrow.celsius.minimumTemperature = tomorrow.mintemp_c + ' °C';
-    this.tomorrow.celsius.averageTemperature = tomorrow.avgtemp_c + ' °C';
-    this.tomorrow.celsius.maximumWindSpeed = tomorrow.maxwind_kph + ' km/h';
-    this.tomorrow.celsius.totalPrecipitation = tomorrow.totalprecip_mm + ' mm';
-
-    // Tomorrow forecast data - Fahrenheit
-    this.tomorrow.fahrenheit = {};
-    this.tomorrow.fahrenheit.maximumTemperature = tomorrow.maxtemp_f + ' °F';
-    this.tomorrow.fahrenheit.minimumTemperature = tomorrow.mintemp_f + ' °F';
-    this.tomorrow.fahrenheit.averageTemperature = tomorrow.avgtemp_f + ' °F';
-    this.tomorrow.fahrenheit.maximumWindSpeed = tomorrow.maxwind_mph + ' mph';
-    this.tomorrow.fahrenheit.totalPrecipitation =
-      tomorrow.totalprecip_in + ' in';
-
-    // Two days forecast data
-    this.twoDays = {};
-    this.twoDays.weatherDescription = twoDays.condition.text;
-    this.twoDays.weatherCode = twoDays.condition.code;
-    this.twoDays.sunrise = new Date(`${twoDaysDate} ${twoDaysAstro.sunrise}`);
-    this.twoDays.sunset = new Date(`${twoDaysDate} ${twoDaysAstro.sunset}`);
-    this.twoDays.totalSnow = twoDays.totalsnow_cm + ' cm';
-    this.twoDays.averageHumidity = twoDays.avghumidity + '%';
-    this.twoDays.chanceOfRain = twoDays.daily_chance_of_rain + '%';
-    this.twoDays.chanceOfSnow = twoDays.daily_chance_of_snow + '%';
-    this.twoDays.uvIndex = twoDays.uv;
-    this.twoDays.airQualityIndex = twoDays.air_quality['gb-defra-index'];
-
-    // Two days forecast data - Celsius
-    this.twoDays.celsius = {};
-    this.twoDays.celsius.maximumTemperature = twoDays.maxtemp_c + ' °C';
-    this.twoDays.celsius.minimumTemperature = twoDays.mintemp_c + ' °C';
-    this.twoDays.celsius.averageTemperature = twoDays.avgtemp_c + ' °C';
-    this.twoDays.celsius.maximumWindSpeed = twoDays.maxwind_kph + ' km/h';
-    this.twoDays.celsius.totalPrecipitation = twoDays.totalprecip_mm + ' mm';
-
-    // Two days forecast data - Fahrenheit
-    this.twoDays.fahrenheit = {};
-    this.twoDays.fahrenheit.maximumTemperature = twoDays.maxtemp_f + ' °F';
-    this.twoDays.fahrenheit.minimumTemperature = twoDays.mintemp_f + ' °F';
-    this.twoDays.fahrenheit.averageTemperature = twoDays.avgtemp_f + ' °F';
-    this.twoDays.fahrenheit.maximumWindSpeed = twoDays.maxwind_mph + ' mph';
-    this.twoDays.fahrenheit.totalPrecipitation = twoDays.totalprecip_in + ' in';
+      // Forecast data - Fahrenheit
+      this[`${daysArray[index]}`].fahrenheit = {};
+      this[`${daysArray[index]}`].fahrenheit.maximumTemperature =
+        dayDataArray[index].maxtemp_f + ' °F';
+      this[`${daysArray[index]}`].fahrenheit.minimumTemperature =
+        dayDataArray[index].mintemp_f + ' °F';
+      this[`${daysArray[index]}`].fahrenheit.averageTemperature =
+        dayDataArray[index].avgtemp_f + ' °F';
+      this[`${daysArray[index]}`].fahrenheit.maximumWindSpeed =
+        dayDataArray[index].maxwind_mph + ' mph';
+      this[`${daysArray[index]}`].fahrenheit.totalPrecipitation =
+        dayDataArray[index].totalprecip_in + ' in';
+    }
   }
 }
