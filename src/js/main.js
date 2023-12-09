43ed31e1b2d7321260423c9bcaeddeb3ae8b42cc
dziubenzo <dziubenzo@gmail.com>
2023-12-09 13:42:31 +0100

    Handle fetch errors

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,23 @@ import '../css/main.scss';
 let location = 'Katowice';
 
 async function getWeatherData(location) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=b9aed0e0fc274fd7b8a152201230712&q=${location}&days=3&aqi=yes&alerts=yes`,
-    { mode: 'cors' },
-  );
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=b9aed0e0fc274fd7b8a152201230712&q=${location}&days=3&aqi=yes&alerts=yes`,
+      { mode: 'cors' },
+    );
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      const error = await response.json();
+      throw new Error(error.error.message);
+    }
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
 }
 
-getWeatherData(location);
+getWeatherData(location).then(data => {
+  console.log(data);
+});
