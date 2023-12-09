19e120a898eb3af40e5f95867d685e4961ab1df1
dziubenzo <dziubenzo@gmail.com>
2023-12-09 16:35:13 +0100

    Build the entire Location class constructor
    It works fine but looks horrendous repetition-wise. The plan is
    to improve it.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 import '../css/main.scss';
 import Location from './location';
 
+// Three days is the default as it is what the free Weather API key provides
+const FORECAST_LENGTH = 3;
 let locationName = 'Katowice';
 
 // Fetch weather data from Weather API
 async function getWeatherData(location) {
   try {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=b9aed0e0fc274fd7b8a152201230712&q=${location}&days=3&aqi=yes&alerts=yes`,
+      `https://api.weatherapi.com/v1/forecast.json?key=b9aed0e0fc274fd7b8a152201230712&q=${location}&days=${FORECAST_LENGTH}&aqi=yes`,
       { mode: 'cors' },
     );
     if (response.ok) {
@@ -23,11 +25,29 @@ async function getWeatherData(location) {
 }
 
 getWeatherData(locationName).then((data) => {
-  const locationData = data.location
-  const currentData = data.current
-  const todayData = data.forecast.forecastday[0].day
-  const tomorrowData = data.forecast.forecastday[1].day
-  const twoDaysData = data.forecast.forecastday[2].day
-  const location = new Location(locationData, currentData, todayData, tomorrowData, twoDaysData);
+  const locationData = data.location;
+  const currentData = data.current;
+  const todayData = data.forecast.forecastday[0].day;
+  const todayAstroData = data.forecast.forecastday[0].astro;
+  const todayDate = data.forecast.forecastday[0].date;
+  const tomorrowData = data.forecast.forecastday[1].day;
+  const tomorrowAstroData = data.forecast.forecastday[1].astro;
+  const tomorrowDate = data.forecast.forecastday[1].date;
+  const twoDaysData = data.forecast.forecastday[2].day;
+  const twoDaysAstroData = data.forecast.forecastday[2].astro;
+  const twoDaysDate = data.forecast.forecastday[2].date;
+  const location = new Location(
+    locationData,
+    currentData,
+    todayData,
+    todayAstroData,
+    todayDate,
+    tomorrowData,
+    tomorrowAstroData,
+    tomorrowDate,
+    twoDaysData,
+    twoDaysAstroData,
+    twoDaysDate,
+  );
   console.log(location);
 });
